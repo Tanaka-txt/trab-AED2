e6bea1589a8b1c91dba8de3c289d1508d0692f85
Tanaka-txt <juliocesarvergamini@gmail.com>
2026-04-01 12:07:28 -0300

    a

--- Codigo/func1.c
+++ Codigo/func1.c
@@ -27,22 +27,17 @@ typedef struct registro_dados {
   int tamNomeEstacao; //                                     ===== 4 bytes
   char *nomeEstacao; //                                      ===== - bytes (Tem que remover 0 \0)
   int tamNomeLinha;//                                        ===== 4 bytes
-  char *nomeLinha;//                                       ===== - bytes (Tem que remover 0 \0)
+  char *nomeLinha;//                                         ===== - bytes (Tem que remover 0 \0)
 }reg_dados;
 
 // BinarioNaTela();
-
-// ____________________________________________________________________________________________________________________________________________________ _________________ _______________
-//|                                                TAMANHO FIXO                                                                      | TAMANHO VARIADO | TAMANHO VARIADO | TAMANHO FIXO  |
-//| removido | proximo | codEstacao | codLinha | codProxEstacao | distProxEstacao | codLinhaIntegra | codEstIntegra | tamNomeEstacao |   Nome Estacao  |  tamNomeLinha   |   Nome Linha  |
-
-
 
 
 // leitura csv, fazer filtro para interpretar cada coluna da tabela ",", filtrar condições se dado pode ser escrito
 
 void read_csv(){
 
+// =-=-= Abrimos o Arquivo CSV =-=-=
   FILE *estacoes = fopen("estacoes.csv", "r"); // csv
   // FILE *bin = fopen("saida.bin", "wb"); // bin escrevo nele 
 
@@ -51,35 +46,66 @@ void read_csv(){
     return;
   }
 
-  char linha[256]; // ele começa no 256 pois ele pula a primeira linha com 255 caracteres + "\0".
-  fgets(linha, 256, estacoes); // faz leitura do arquivo .csv
+  char linha[256]; // ele começa no 256 pois ele pula a primeira linha com 255 caracteres + "\0". 256+256 =  512 (BUFFER!!!)
+  char *ptr = linha; // loop linha += linha????
+  reg_dados registro[1000];
+  int i = 0;
 
-  // Para teste de leitura do arquivo.csv
-    // while (fgets(linha, 256, estacoes)) {
-    //   printf("%s", linha); // só pra testar
-    // }
+  fgets(linha, 256, estacoes); // Pula o cabeçalho do csv
 
-  // char *campo = strtok(256,",");//Vamos receber o endereço de cada campo do registro separadamente, a gente usa o strtok para fazer leitura em partes de string, onde o delimitador é o separador strtok(caracteres da linha, token)
-  while (fgets(linha, 256, estacoes)) {
+  while (fgets(linha, 256, estacoes)) { // faz o loop para leitura das linhas do csv
+    char *ptr = linha;
 
-    char *token;
+    registro[i].codEstacao = atoi(strsep(&ptr, ","));
+    registro[i].nomeEstacao = strsep(&ptr,",");
+    registro[i].codLinha = atoi(strsep(&ptr,","));
+    registro[i].nomeLinha = strsep(&ptr,",");
+    registro[i].codProxEstacao = atoi(strsep(&ptr,","));
+    registro[i].distProxEstacao = atoi(strsep(&ptr,","));
+    registro[i].codLinhaIntegra = atoi(strsep(&ptr,","));
+    registro[i].codEstIntegra = atoi(strsep(&ptr,","));
 
-    token = strtok(linha, ",");
+      // campo pode ser vazio
+      char *campo = strsep(&ptr, ",");
+      if (campo == NULL || strlen(campo) == 0)
+          registro[i].codLinhaIntegra = -1;
+      else
+          registro[i].codLinhaIntegra = atoi(campo);
+
+      campo = strsep(&ptr, ",");
+      if (campo == NULL || strlen(campo) == 0)
+          registro[i].codEstIntegra = -1;
+      else
+          registro[i].codEstIntegra = atoi(campo);
+
+    // Debug
+    printf("%d | %s | %d | %s | %d | %d | %d | %d\n", 
+    registro[i].codEstacao,
+    registro[i].nomeEstacao,
+    registro[i].codLinha,
+    registro[i].nomeLinha,
+    registro[i].codProxEstacao,
+    registro[i].distProxEstacao,
+    registro[i].codLinhaIntegra,
+    registro[i].codEstIntegra);
     
-    int codEstacao = atoi(token);
-
-    token = strtok(NULL, ",");
-    int codLinha = atoi(token);
-
-    token = strtok(NULL, ",");
-    char *nomeEstacao = token;
-
-    token = strtok(NULL, ",");
-    char *nomeLinha = token;
-
-    printf("Cod: %d | Linha: %d | Estacao: %s | NomeLinha: %s\n",
-           codEstacao, codLinha, nomeEstacao, nomeLinha);
+    i++;
   }
+
+  
+
+  // printf("nomeEstacao: %s\n", registro[1].nomeEstacao);
+  // printf("codLinha: %d\n", registro[1].codLinha);
+  // printf("nomeLinha: %s\n", registro[1].nomeLinha);
+  // printf("codProxEstacao: %d\n", registro[1].codProxEstacao);
+  // printf("distProxEstacao: %d\n", registro[1].distProxEstacao);
+  // printf("codLinhaIntegra: %d\n", registro[1].codLinhaIntegra);
+  // printf("codEstIntegra: %d\n", registro[1].codEstIntegra);
+
+
+  // Fazer loop para leitura até o fim do arquivo
+  // Dentro dele tem que fazer um loop para rodar por linha para anotar cada campo por registro
+  // a,,c podemos usar para verificar esses vazios com RTSTOK???
 
 
   // fclose("estacoes.csv"); // Fecha arquivo
