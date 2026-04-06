96b67d142ec574687ead6693f27e86d0331f6e0f
Laysa <laysaoliveir@gmail.com>
2026-04-06 10:06:47 -0300

    add comentários

--- Codigo/impressao.c
+++ Codigo/impressao.c
@@ -1,14 +1,21 @@
+/*
+- para cada linha válida, imprimir os dados em uma única linha no terminal, separando tudo por um espaço em branco
+- a ordem de impressão tem que ser exatamente esta: código da estação, nome da estação, código da linha, nome da linha, código da próxima estação, distância, código da integração e, por fim, a estação de integração.
+- antes do nome da estação tem um nº que indica o tamanho daquele nome --> se for 0 imprime NULO
+- como 'registro' é um ponteiro para a struct, foi usado o operador de seta '->' para acessar os campos internos
+*/
+
 #include "features.h"
 
-void imprimir_registro (reg_dados *registro){
-
+void imprimir_registro (reg_dados *registro){ // recebe um ponteiro (*registro) do tipo reg_dados
+    
     printf("%d %s ", registro->codEstacao, registro->nomeEstacao); // nunca vão ser nulas, por isso não precisa de verificação
 
-    // campos fixos que tiverem o valor nulo, em vez de ser -1 tem q printar NULO
-    // campos variáveis que tiverem o valor nulo, devem printar NULO
+    // campos fixos que tiverem o valor nulo, recebem o valor -1 na leitura e tem que printar NULO
+    // campos variáveis que tiverem o valor nulo, tem indicador de tamanho igua a 0 e devem printar NULO
 
     if (registro->codLinha == -1) {
-        printf("NULO ");
+        printf("NULO "); // substitui a impressão do -1 pela palavra "NULO"
     } else{
         printf("%d ", registro->codLinha);
     }
@@ -16,7 +23,7 @@ void imprimir_registro (reg_dados *registro){
     if (registro->tamNomeLinha == 0){
         printf("NULO ");
     }else {
-        printf("%s ", registro->nomeLinha);
+        printf("%s ", registro->nomeLinha); // se o tamanho for maior que 0, a string existe e pode ser impressa
     }
 
     if (registro->codProxEstacao == -1){
@@ -43,3 +50,4 @@ void imprimir_registro (reg_dados *registro){
         printf("%d\n", registro->codEstIntegra);
     }
 }
+
--- Codigo/leitura.c
+++ Codigo/leitura.c
@@ -1,20 +1,17 @@
 #include "features.h"
 
-int ler_registro(FILE *binario, reg_dados *registro){
-    // como 'registro' foi recebido como ponteiro (*), usamos a seta '->' para acessar seus campos 
+int ler_registro(FILE *binario, reg_dados *registro){ // como 'registro' foi recebido como ponteiro (*), foi usado a seta '->' para acessar os campos na struct original
     // & indica o endereço de memória onde o fread deve salvar o dado lido
     
     if(fread(&registro->status_removido, sizeof(char), 1, binario) == 0){
-        return 0; // significa que o arquivo acabou e retorna 0
+        return 0; // significa que o arquivo acabou e retorna 0 para quebrar o laço (break) na função principal
     }
 
-    if(registro->status_removido == '1'){ // significa que o registro foi apagado depois de ler o 1º byte
+    if(registro->status_removido == '1'){ // significa que o registro foi apagado depois de ler o 1º byte e a linha será pulada
         fseek(binario, 79, SEEK_CUR);     // como já leu o 1º pula o restante (79 bytes a partir da posição atual) indo para a próxima linha
-        // printf("Registro inexistente"); // 👀
-        return 2;                         // significa que o arquivo foi apagado e a linha será pulada👀
     }
 
-    fseek(binario, 4, SEEK_CUR); // pula Próximo RRN
+    fseek(binario, 4, SEEK_CUR); // pula os 4 bytes do campo "próximo" (RRN do próximo registro removido)
 
     // leitura campo a campo, na ordem que aparece no arquivo --> 6 inteiros de 4 bytes
     fread(&registro->codEstacao, sizeof(int), 1, binario);
@@ -24,12 +21,12 @@ int ler_registro(FILE *binario, reg_dados *registro){
     fread(&registro->codLinhaIntegra, sizeof(int), 1, binario);
     fread(&registro->codEstIntegra, sizeof(int), 1, binario);
 
-    // ------- 👀
-    fread(&registro->tamNomeEstacao, sizeof(int), 1, binario);
+    // leitura dos campos de tamanho variável
+    fread(&registro->tamNomeEstacao, sizeof(int), 1, binario); // lê primeiro o tamanho da string
         if (registro->tamNomeEstacao > 0) { 
-            registro->nomeEstacao = malloc(registro->tamNomeEstacao + 1); 
-            fread(registro->nomeEstacao, sizeof(char), registro->tamNomeEstacao, binario);    
-            registro->nomeEstacao[registro->tamNomeEstacao] = '\0'; 
+            registro->nomeEstacao = malloc(registro->tamNomeEstacao + 1);                   // malloc aloca memória dinâmica, o +1 garante espaço para o caractere ('\0')
+            fread(registro->nomeEstacao, sizeof(char), registro->tamNomeEstacao, binario);  // lê a quantidade de letras especificada, sem ler o lixo do arquivo 
+            registro->nomeEstacao[registro->tamNomeEstacao] = '\0';                         // insere o '\0' para que printf e strcmp funcionem na memória, já que o arquivo não armazena esse caractere
         }
 
     fread(&registro->tamNomeLinha, sizeof(int), 1, binario);
@@ -41,15 +38,14 @@ int ler_registro(FILE *binario, reg_dados *registro){
     // ------- 👀
     
     // somando apenas os bytes fixos: 1 do removido, 4 do próximo, 24 do bloco fico, 4 do tamNomeEstacao e 4 tamNomeLinha = 37 bytes
-    // dos 80 bytes, 37 são fixos e temos a quant. de letras da estação e linha, o resto é o lixo de memória ($) que vai ser pulado
+    // dos 80 bytes, 37 são fixos e temos a quantidade de letras da estação e linha, o resto é o lixo de memória ($) que vai ser pulado
     int bytesrestantes = 80 - 37 - registro->tamNomeEstacao - registro->tamNomeLinha;
     fseek(binario, bytesrestantes, SEEK_CUR); // fseek(arquivo, tamanho_do_pulo_em_bytes_do_lixo, a_partir_da_posição_atual_do_cursor)
     
     return 1; // retorna 1 se leu com sucesso
 }
 
-/* 
-a seta '->' está no lugar '.' porque a struct não foi criada aqui 
-a fread recebe apenas um ponteiro (*), então a seta acessa a memória da struct que está guardada em outro lugar
-o'&' passa esse endereço exato para o fread
+/*  a seta '->' está no lugar '.' porque a struct não foi criada aqui 
+    a fread recebe apenas um ponteiro (*), então a seta acessa a memória da struct que está guardada em outro lugar
+    o'&' passa esse endereço exato para o fread
 */
